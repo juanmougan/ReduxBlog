b847a255ff8821b2d154be72ac1e477ff28fcdaa
Juan Manuel Mougan <juanmougan@gmail.com>
2018-03-19 13:48:07 -0300

    Started with router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Router } from 'react-router-dom'
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
+
+//path is: if the user goes to this path, I'll show this component
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
