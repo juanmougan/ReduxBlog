eb6241d02dcc83b367f1f69da4b65a86c349e023
Juan Manuel Mougan <juanmougan@gmail.com>
2018-03-19 23:54:30 -0300

    Added code for action

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom'
+import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// redux-promise as a middleware
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 //path is: if the user goes to this path, I'll show this component
 ReactDOM.render(
