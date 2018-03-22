9c465b1635cee86641f0ab50b9a5cab18c2bb483
Juan Manuel Mougan <juanmougan@gmail.com>
2018-03-21 23:38:04 -0300

    Implemented a reducer for posts

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import { FETCH_POSTS } from '../actions';
+
+// for the first time it runs, we're going to default state to an empty object
+exports default function (state = {}, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      // If I did this here
+      //console.log(action.payload.data);     // I'd get [ post1, post2 ]
+      // I want to transform this into
+      // something like { id: post }
+      // E.g. { 4: post }
+      return _.mapKeys(action.payload.data, 'id');
+    default:
+      return state;
+  }
+};
