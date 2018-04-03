81d28c9c9e32f6decfc1d949d0af9c078b359e3a
Juan Manuel Mougan <juanmougan@gmail.com>
2018-04-03 11:51:56 -0300

    Added navigation to new post

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+// Import the "reducer property" with an alias of "formReducer"
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts'
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  // Important: assign to the key "form" - others reducers use this property
+  form: formReducer
 });
 
 export default rootReducer;
