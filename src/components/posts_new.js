7ff9c6954840f58dc54370295e5f9290ac799d3a
Juan Manuel Mougan <juanmougan@gmail.com>
2018-07-10 19:11:48 -0300

    Started with Field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'react-form';
 
 class PostsNew extends Component {
   render() {
     return(
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          name="title"
+          component={}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// the function passed to reduxForm is something similar to the
+// mapStateToProps in the connect function
+export default reduxForm({
+  form: 'PostsNewForm'    // has to be an unique string
+                          // so react-form can handle multiple forms
+                          // I can use this string elsewhere, and the state will be merged
+})(PostsNew);
